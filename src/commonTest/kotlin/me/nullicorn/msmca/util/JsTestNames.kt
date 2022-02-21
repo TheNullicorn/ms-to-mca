ed65e29df42f7b7d5065ed9b7632f895576b17b7
Nullicorn <bennullicorn@gmail.com>
2022-02-21 14:43:21 -0500

    Revise reasoning for JsTestNames file
    Signed-off-by: Nullicorn <bennullicorn@gmail.com>

--- src/commonTest/kotlin/me/nullicorn/msmca/util/JsTestNames.kt
+++ src/commonTest/kotlin/me/nullicorn/msmca/util/JsTestNames.kt
@@ -1,14 +1,29 @@
 package me.nullicorn.msmca.util
 
-/*
- * Nondescript names for use in the [JsName] annotation on tests named using back-ticks (`).
+import kotlin.js.JsName
+
+/**
+ * Nondescript names for the [@JsName][JsName] annotation, particularly on tests whose function names are
+ * wrapped in backticks, e.g. &#96;...&#96;.
  *
- * Kotlin/JS doesn't allow functions with backticks to include special characters (spaces,
- * punctuation, symbols) unless you annotate the function with @JsName and a normal name.
+ * Usage:
+ * ```kotlin
+ * @Test
+ * @JsName(ONE)
+ * fun `should do something really cool`() {
+ *     // Your awesome test here...
+ * }
+ * ```
+ *
+ * # Explanation
+ *
+ * Kotlin/JS doesn't allow functions named with backticks to include special characters (spaces,
+ * punctuation, symbols) unless you annotate those functions with @JsNames that include valid
+ * function names.
  *
  * However, the IDE (or at least IntelliJ) still displays the test's correct name in the testing UI,
  * so the `@JsName` doesn't need to be anything meaningful. Hence, the values included here can be
- * used to give tests unique [JsName] values.
+ * used to give tests unique [@JsName][JsName] values.
  *
  * If you're contributing tests and need more unique names, feel free to add them as-needed.
  */
